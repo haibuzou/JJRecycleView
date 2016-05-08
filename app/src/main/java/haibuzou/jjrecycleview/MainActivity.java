15bd81bf94d0a54437d282c23e842bc531667a26
haibuzou <haibuzouxi@163.com>
2016-05-08 21:44:53 +0800

    init commit

--- app/src/main/java/haibuzou/jjrecycleview/MainActivity.java
+++ app/src/main/java/haibuzou/jjrecycleview/MainActivity.java
@@ -2,12 +2,18 @@ package haibuzou.jjrecycleview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecycleAdapter recycleAdapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyle_view);
+
     }
 }
